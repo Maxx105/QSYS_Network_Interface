5ca9c29ee6cf3668c58449bb80661ac0cea31f12
Maxx Sanner <maxxsanner105@gmail.com>
2022-02-10 09:39:02 -0500

    added basic plugin framework

---
+++ info.lua
@@ -0,0 +1,8 @@
+PluginInfo = {
+  Name = "My Plugins~Network Interface",
+  Version = "0.0",
+  BuildVersion = "0.0.0.0",
+  Id = "799af688-8c3d-48e7-b34a-d44de2018716",
+  Author = "Maxx Sanner",
+  Description = "A plugin that allows to make network changes to any Core on the network"  
+}
---
+++ layout.lua
@@ -0,0 +1,10 @@
+local CurrentPage = PageNames[props["page_index"].Value]
+if CurrentPage == "Lan A" then
+ 
+elseif CurrentPage == "Lan B" then
+
+elseif CurrentPage == "Aux A" then
+
+elseif CurrentPage == "Aux B" then
+  
+end
